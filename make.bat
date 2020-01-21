b5e9d2f7b1c507b82e01538e54e7887a64139fd3
Peter Lohse <peterlohse@yahoo.de>
2020-01-21 12:21:50 +0100

    initial commit

--- make.bat
+++ make.bat
@@ -1,9 +1,5 @@
 @ECHO OFF
-rm -f acptemplates.tar
-7z a -ttar -mx=9 acptemplates.tar .\acptemplates\*
 del files.tar
 7z a -ttar -mx=9 files.tar .\files\*
-del templates.tar
-7z a -ttar -mx=9 templates.tar .\templates\*
-del dev.hanashi.wsc.template1.tar
-7z a -ttar -mx=9 dev.hanashi.wsc.template1.tar .\* -x!acptemplates -x!files -x!templates -x!dev.hanashi.wsc.template1.tar -x!.git -x!.gitignore -x!make.bat -x!make.sh
+del dev.hanashi.wsc.steam-api.tar
+7z a -ttar -mx=9 dev.hanashi.wsc.steam-api.tar .\* -x!acptemplates -x!files -x!templates -x!dev.hanashi.wsc.steam-api.tar -x!.git -x!.gitignore -x!make.bat -x!make.sh
