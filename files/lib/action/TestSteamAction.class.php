9e703d516142632901d11ac81a6e95377e3bbe49
Peter Lohse <peterlohse@yahoo.de>
2020-01-22 18:19:29 +0100

    OpenID korrekt eingebaut

--- files/lib/action/TestSteamAction.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace wcf\action;
-use wcf\system\steam\SteamAPI;
-
-class TestSteamAction extends AbstractAction {
-    public function execute() {
-        wcfDebug(SteamAPI::getAssetPrices(440, 'EUR', 'de'));
-    }
-}
