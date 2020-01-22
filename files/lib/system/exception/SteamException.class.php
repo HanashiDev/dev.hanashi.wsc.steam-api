decf39283c7bf783a94d551940fab28f5c7008d4
Peter Lohse <peterlohse@yahoo.de>
2020-01-22 08:26:03 +0100

    Dokumentation

---
+++ files/lib/system/exception/SteamException.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace wcf\system\exception;
+
+/**
+* Steam exception
+*
+* @author Peter Lohse <hanashi@hanashi.eu>
+* @copyright Hanashi Development
+* @license	Freie Lizenz (https://hanashi.eu/freie-lizenz/)
+* @package	WoltLabSuite\Core\System\Exception
+*/
+class SteamException extends SystemException {}
--- files/lib/system/steam/SteamAPI.class.php
+++ files/lib/system/steam/SteamAPI.class.php
@@ -1,68 +1,187 @@
 <?php
 namespace wcf\system\steam;
+use wcf\system\exception\SteamException;
+use wcf\util\HTTPRequest;
+use wcf\util\JSON;
 
+/**
+ * Steam Web Api class
+ * api key can got from here: https://steamcommunity.com/dev/apikey
+ * method documentation is from original documentations
+ * 
+ * @see https://steamcommunity.com/dev
+ * @see https://openid.net/specs/openid-authentication-2_0.html
+ * @author Peter Lohse <hanashi@hanashi.eu>
+ * @copyright Hanashi Development
+ * @license	Freie Lizenz (https://hanashi.dev/freie-lizenz/)
+ * @package	WoltLabSuite\Core\System\Steam
+ */
 class SteamAPI {
-	// TODO: Testschlüssel: 411C7A63AF279374C9D8554B898AC0B5
-	// https://steamcommunity.com/dev/apikey
-
-	// https://steamcommunity.com/dev?l=german
-
-	public static function getNewsForApp($appID, $count = 10, $maxLength = 300) {
-
-	}
-
-	public static function getGlobalAchievmentPercentagesForApp($appID) {
-
-	}
-
-	public static function getGlobalStatsForGame($appID, $names, $count = 1) {
+	/**
+	 * returns the latest news of a game specified by its appID
+	 * 
+	 * @var 	int		$appID		AppID of the game you want the news of.
+	 * @var 	int		$count 		(optional) How many news enties you want to get returned.
+	 * @var 	int		$maxLength	(optional) Maximum length of each news entry.
+	 * @return 	array	latest news of a game
+	 */
+	public static function getNewsForApp(int $appID, int $count = null, int $maxLength = null) : array {
 
 	}
 
-	public static function getPlayerSummaries($steamIDs) {
+	/**
+	 * returns global achievements overview of a specific game in percentages
+	 * 
+	 * @var		int		$appID		AppID of the game you want the percentages of.
+	 * @return	array	global achievements overview of a specific game in percentages
+	 */
+	public static function getGlobalAchievmentPercentagesForApp(int $appID) : array {
 
 	}
 
-	public static function getFriendList($steamID, $relationShip = 'friend') {
+	/**
+	 * returns global stats of a specific game
+	 * 
+	 * @var		int		$appID		AppID of the game you want the stats of
+	 * @var		array	$names		Name of the achievement as defined in Steamworks. (name[0] [and name[1], etc.])
+	 * @var		int		$count		Length of the array of global stat names you will be passing.
+	 * @return	array	global stats of a specific game
+	 */
+	public static function getGlobalStatsForGame(int $appID, array $names, int $count = 1) : array {
 
 	}
 
-	public static function getPlayerAchievments($steamID, $appID, $language = null) {
+	/**
+	 * returns basic profile information for a list of 64-bit Steam IDs
+	 * 
+	 * @var		array	$steamIDs	List of 64 bit Steam IDs to return profile information for. Up to 100 Steam IDs can be requested.
+	 * @return	array	basic profile information for a list of 64-bit Steam IDs (Some data associated with a Steam account may be hidden if the user has their profile visibility set to "Friends Only" or "Private". In that case, only public data will be returned.)
+	 */
+	public static function getPlayerSummaries(array $steamIDs) : array {
 
 	}
 
-	public static function getUserStatsForGame($steamID, $appID, $language = null) {
+	/**
+	 * returns the friend list of any Steam user, provided their Steam Community profile visibility is set to "Public"
+	 * 
+	 * @var		int		$steamID		64 bit Steam ID to return friend list for
+	 * @var		string	$relationShip	Relationship filter. Possibles values: all, friend
+	 * @return	array	friend list of any Steam user, provided their Steam Community profile visibility is set to "Public"
+	 */
+	public static function getFriendList(int $steamID, string $relationShip = 'friend') : array {
 
 	}
 
-	public static function getOwnedGames($steamID, $includeAppInfo = true, $includeFreeGames = false, $appIDs = []) {
+	/**
+	 * returns a list of achievements for this user by app id
+	 * 
+	 * @var		int		$steamID		64 bit Steam ID to return achievements for.
+	 * @var		int		$appID			The ID for the game you're requesting
+	 * @var		string	$language		(optional) Language. If specified, it will return language data for the requested language.
+	 * @return	array	list of achievements for this user by app id
+	 */
+	public static function getPlayerAchievments(int $steamID, int $appID, string $language = null) : array {
 
 	}
 
-	public static function getRecentlyPlayedGames($steamID, $count = null) {
+	/**
+	 * returns a list of stats for this user by app id
+	 * 
+	 * @var		int		$steamID		64 bit Steam ID to return stats for.
+	 * @var		int		$appID			The ID for the game you're requesting
+	 * @var		string	$language		(optional) Language. If specified, it will return language data for the requested language.
+	 * @return	array	list of stats for this user by app id
+	 */
+	public static function getUserStatsForGame(int $steamID, int $appID, string $language = null) : array {
 
 	}
 
-	public static function isPlayingSharedGame($steamID, $appIDPlaying) {
+	/**
+	 * returns a list of games a player owns along with some playtime information, if the profile is publicly visible. Private, friends-only, and other privacy settings are not supported unless you are asking for your own personal details (ie the WebAPI key you are using is linked to the steamid you are requesting).
+	 * 
+	 * @var		int		$steamID			The SteamID of the account.
+	 * @var		bool	$includeAppInfo		(optional) Include game name and logo information in the output. The default is to return appids only.
+	 * @var		bool	$includeFreeGames	(optional) By default, free games like Team Fortress 2 are excluded (as technically everyone owns them). If include_played_free_games is set, they will be returned if the player has played them at some point. This is the same behavior as the games list on the Steam Community.
+	 * @var		array	$appIDs				(optional) You can optionally filter the list to a set of appids.
+	 * @return	array	list of games a player owns along with some playtime information
+	 */
+	public static function getOwnedGames(int $steamID, bool $includeAppInfo = true, bool $includeFreeGames = false, array $appIDs = []) : array {
 
 	}
 
-	public static function getSchemaForGame($appID, $language = null) {
+	/**
+	 * returns a list of games a player has played in the last two weeks, if the profile is publicly visible. Private, friends-only, and other privacy settings are not supported unless you are asking for your own personal details (ie the WebAPI key you are using is linked to the steamid you are requesting).
+	 * 
+	 * @var		int		$steamID	The SteamID of the account.
+	 * @var		int		$count		(optional) Optionally limit to a certain number of games (the number of games a person has played in the last 2 weeks is typically very small)
+	 * @return	array	list of games a player has played in the last two weeks
+	 */
+	public static function getRecentlyPlayedGames(int $steamID, int $count = null) : array {
 
 	}
 
-	public static function getPlayerBans($steamIDs) {
+	/**
+	 * returns the original owner's SteamID if a borrowing account is currently playing this game. If the game is not borrowed or the borrower currently doesn't play this game, the result is always 0.
+	 * 
+	 * @var		int		$steamID		The SteamID of the account playing.
+	 * @var		int		$appIDPlaying	The AppID of the game currently playing
+	 * @return	int		original owner's SteamID if a borrowing account is currently playing this game
+	 */
+	public static function isPlayingSharedGame(int $steamID, int $appIDPlaying) : int {
 
 	}
 
-	// openID
-	// https://openid.net/specs/openid-authentication-2_0.html
+	/**
+	 * returns gamename, gameversion and availablegamestats (achievements and stats).
+	 * 
+	 * @var		int			$appID		The AppID of the game you want stats of
+	 * @var		string		$language	(optional) Language. If specified, it will return language data for the requested language.
+	 * @return	array		gamename, gameversion and availablegamestats (achievements and stats).
+	 */
+	public static function getSchemaForGame(int $appID, string $language = null) : array {
 
-	public static function getOpenIDUrl($redirectUri, $realm) {
-		// https://steamcommunity.com/openid/login?openid.ns=http://specs.openid.net/auth/2.0&openid.mode=checkid_setup&openid.return_to=https://hanashi.dev/steam/test.php&openid.realm=https://hanashi.dev&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select&openid.identity=http://specs.openid.net/auth/2.0/identifier_select
 	}
 
-	public static function validateOpenID() {
-		// https://hanashi.dev/steam/test.php?openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.mode=id_res&openid.op_endpoint=https%3A%2F%2Fsteamcommunity.com%2Fopenid%2Flogin&openid.claimed_id=https%3A%2F%2Fsteamcommunity.com%2Fopenid%2Fid%2F76561198049434265&openid.identity=https%3A%2F%2Fsteamcommunity.com%2Fopenid%2Fid%2F76561198049434265&openid.return_to=https%3A%2F%2Fhanashi.dev%2Fsteam%2Ftest.php&openid.response_nonce=2020-01-21T12%3A41%3A54Zc4Xs%2BIFl6Up6Sr8JdU84JV4Y2n4%3D&openid.assoc_handle=1234567890&openid.signed=signed%2Cop_endpoint%2Cclaimed_id%2Cidentity%2Creturn_to%2Cresponse_nonce%2Cassoc_handle&openid.sig=GbKmtwudO%2B9WOUtvttG%2FZMFF0ts%3D
+	/**
+	 * returns Community, VAC, and Economy ban statuses for given players.
+	 * 
+	 * @var		array		list of SteamIDs
+	 * @return	array		Community, VAC, and Economy ban statuses for given players
+	 */
+	public static function getPlayerBans(array $steamIDs) : array {
+
+	}
+
+	/**
+	 * get OpenID login url
+	 * 
+	 * @var 	string	$redirectUri	URL to which the OP SHOULD return the User-Agent with the response indicating the status of the request.
+	 * @var 	string	$realm			URL pattern the OP SHOULD ask the end user to trust.
+	 * @return 	string
+	 */
+	public static function getOpenIDUrl(string $redirectUri, string $realm) : string {
+		$data = [
+			'openid.ns' => 'http://specs.openid.net/auth/2.0',
+			'openid.mode' => 'checkid_setup',
+			'openid.return_to' => $redirectUri,
+			'openid.realm' => $realm,
+			'openid.claimed_id' => 'http://specs.openid.net/auth/2.0/identifier_select',
+			'openid.identity' => 'http://specs.openid.net/auth/2.0/identifier_select'
+		];
+		return 'https://steamcommunity.com/openid/login?' . http_build_query($data, null, '&');
+	}
+
+	/**
+	 * validate OpenID data and returns SteamID
+	 * 
+	 * @var		array	$data	content of $_GET by redirect uri, where prefix is "openid."
+	 * @return	int		64 bit of SteamID
+	 */
+	public static function validateOpenID(array $data) : int {
+
+	}
+
+	protected static function execute($url, $data) {
+
 	}
 }
