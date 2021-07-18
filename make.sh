23c232390cebf695ec8b8d35b0b722ef911c25e1
Peter Lohse <github@hsrvmail.net>
2021-07-18 10:54:19 +0200

    Buildscript

--- make.sh
+++ make.sh
@@ -9,7 +9,7 @@ do
 done
 
 rm -rf ${PACKAGE_NAME}.tar ${PACKAGE_NAME}.tar.gz
-7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.phpcs.xml -x!.github -x!ts -x!node_modules -x!package-lock.json -x!package.json -x!tsconfig.json
+7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.phpcs.xml -x!.php_cs.dist -x!.github -x!ts -x!node_modules -x!package-lock.json -x!package.json -x!tsconfig.json
 
 for i in "${PACKAGE_TYPES[@]}"
 do
